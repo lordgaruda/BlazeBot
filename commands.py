a84bffd30f9328ee3e1a61181c1ffbd332013e9e
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2022-10-07 00:46:34 +0530

    Update screenshot link

--- commands.py
+++ commands.py
@@ -129,7 +129,7 @@ Please make device changelog file for {codename} <a href="https://github.com/Pro
 
 ▪️ Changelog:</strong> <a href="https://github.com/ProjectBlaze/official_devices/blob/12.1/changelog.md" >Source</a> | <a href="{dclog}" >Device</a>
 ▪️ <a href="https://www.projectblaze.live/" >Download</a>
-▪️ <a href="https://t.me/projectblaze/45830" >Screenshots</a>
+▪️ <a href="https://t.me/projectblaze/48585" >Screenshots</a>
 ▪️ <a href="{database[codename]['sgroup']}" >Support Group</a>
 ▪️ <a href="https://t.me/projectblaze" >Community Chat</a>
 ▪️ <a href="https://t.me/projectblazeupdates" >Updates Channel</a>
